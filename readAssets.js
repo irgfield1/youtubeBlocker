32a4c0f78632b41eed67098234f448663333dfeb
Isaac G <isaac.garfield@inferno-systems.com>
2022-01-13 08:26:05 -0500

    BLOCK:= type parser of .txt file

--- readAssets.js
+++ readAssets.js
@@ -1,16 +1,37 @@
 
-
+let youtubeVideoPattern = /(http(s)??\:\/\/)?(www\.)?((youtube\.com\/watch\?v=)|(youtu.be\/))([a-zA-Z0-9\-_])+/;
 
 function interpret(data) {
     if (typeof data == "undefined") {
         console.log("nodata");
         return;
     } else {
-        console.log(data);
+        let j = data.split(';');
+        console.log(j);
+        let blocks = j[0].trim();
+        let allows = j[1].trim();
+        console.log(blocks);
+        console.log(allows);
+        console.log(arrayify(blocks));
+        console.log(arrayify(allows));
+        let contentToStore = { arrayify(blocks), arrayify(allows) }
         return;
     }
 }
 
+function arrayify(myString) {
+    let contentToStore = {}
+    let blockStatus = [myString.substring(myString.indexOf(":="), -1)];
+    myString = myString.substring(myString.indexOf(":=") + 2).trim();
+    myString = myString.split(",").map(s => s.trim());
+    contentToStore[blockStatus] = myString;
+    return contentToStore;
+}
+
+function objectify(array1, array2) {
+
+
+}
 
 function fetchStorage(textAsset = "assets/blockfolder/data.txt") {
     // let textAsset = "assets/blockfolder/data.txt";
@@ -21,7 +42,8 @@ function fetchStorage(textAsset = "assets/blockfolder/data.txt") {
             let j = data.body.getReader();
             let k = await j.read()
             const utf8Decoder = new TextDecoder('utf-8');
-            console.log(utf8Decoder.decode(k?.value));
-
+            let l = utf8Decoder.decode(k?.value);
+            interpret(l);
         });
+
 }
