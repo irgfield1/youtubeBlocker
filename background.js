3e2e66b7d672e6a08f2cd117ac6369d8ecc3086e
Isaac G <isaac.garfield@inferno-systems.com>
2022-02-22 15:56:42 -0500

    Cleanup background.js

--- background.js
+++ background.js
@@ -1,4 +1,11 @@
-//const { contextualIdentities } = require("webextension-polyfill");
+/**
+ * The purpose of the background script is block or allow requests to googlevideo urls,
+ * the only other task it has is to add new youtube videos to the history list
+ * 
+ * The browser.tabs.onActivated and onUpdated events set proxy behavior,
+ * then handle proxy request manages the web requests themselves
+ * 
+ */
 let youtubeVideoPattern =
     /(http(s)??\:\/\/)?(www\.)?((youtube\.com\/watch\?v=)|(youtu.be\/))([a-zA-Z0-9\-_])+/;
 let radioStatus;
@@ -8,8 +15,7 @@ let radioStatus;
 //map entry creation for url, defaults to allow on pageload
 async function write2Browser() {
     radioStatus = await radioChangeListener();
-    console.log(radioStatus);
-    if (radioStatus == "blacklist" || radioStatus == "whitelist") {
+    if (radioStatus != "split") {
         return;
     }
     browser.tabs.query({ active: true }).then(async (tabs) => {
@@ -22,78 +28,58 @@ async function write2Browser() {
 }
 
 function greenTab() {
-    console.log("greenTab init");
     browser.tabs.query({ active: true }).then(async (tabs) => {
         let tempUrl = tabs[0].url.slice();
         let tab = tabs[0];
-        console.log(tempUrl);
         if (trimYoutubeUrl(tempUrl) == "Fail") {
             return;
         }
         await readLocalStorage(trimYoutubeUrl(tempUrl)).then(async (data) => {
-            //Success case
-            console.log(data);
-            if (data[0] == "block") {
-                // browser.tabs.sendMessage(tab.id, { cover: true });
-                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                    console.log(tabs);
-                    chrome.tabs.sendMessage(tabs[0]?.id, { cover: true }, function (response) {
-                        console.log(response);
-                    });
-                });
-                // coverGreen();
-            } else if (data[0] == "allow") {
-                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                    chrome.tabs.sendMessage(tabs[0]?.id, { cover: false }, function (response) {
-                        console.log(response);
-                    });
-                });
-            }
+            //In storage
+            chrome.tabs.sendMessage(tab?.id, { cover: data[0] == "block" });
         },
-            async (errInfo) => {
-                //failure case
+            async () => {
+                //Not in storage
                 radioStatus = await radioChangeListener();
-                if (radioStatus == "blacklist") {
-                    browser.tabs.sendMessage(tab.id, { cover: false });
-                } else if (radioStatus == "whitelist") {
-                    browser.tabs.sendMessage(tab.id, { cover: true })
-                    // coverGreen();
+                if (radioStatus == "whitelist") {
+                    browser.tabs.sendMessage(tab.id, { cover: true });
+                } else {
+                    browser.tabs.sendMessage(tab.id, { cover: false })
                 }
             });
     });
 }
 
 async function blockCheck() {
-    console.log("blockCheck");
     //check url using tabs,
     //apply rule
     //detect changes in url
     //add or remove rule...
-    let tab = await getCurrentTab();
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    console.log(tab);
     if (typeof tab == "undefined" || typeof tab.url == "undefined") {
         return;
     }
-    console.log(tab);
     if (youtubeVideoPattern.test(tab.url)) {
-        console.log("Matches regex");
+        console.log("Youtube");
 
         await readLocalStorage(trimYoutubeUrl(tab.url)).then(
             (data) => {
-                console.log(data); //block/allow
-                //base rule
+                console.log(data[0]);
                 setBrowserRules(data[0]);
 
                 chrome.declarativeNetRequest
-                    .getDynamicRules()
-                    .then((data) => console.log(data));
+                    .getDynamicRules();
             },
             async (data) => {
                 //current url not found in storage
                 radioStatus = await radioChangeListener();
-                if (radioStatus == "blacklist") {
-                    setBrowserRules("allow");
-                } else if (radioStatus == "whitelist") {
+                console.log(radioStatus);
+                if (radioStatus == "whitelist") {
                     setBrowserRules("block");
+                } else {
+                    setBrowserRules("allow");
                 }
             }
         );
@@ -103,26 +89,41 @@ async function blockCheck() {
 }
 /**************HELPERS**************************/
 
-function trimYoutubeUrl(url) {
-    console.log(url);
-    let trimUrl;
-    if (youtubeVideoPattern.test(url)) {
-        trimUrl = url.slice(url.search("v="))
-    } else if (url.length == 11) {
-        trimUrl = "v=" + url;
-    } else if (url.length == 13) {
-        trimUrl = url;
-    } else if (url.length > 13) {
-        console.log(`${url} is too long, should be "v=" and the next 11 chars`);
-        return "Fail"
-    } else if (url.length < 11 || url.length == 12) {
-        console.log(`${url} is too short, should be "v=" and the next 11 chars`);
-        return "Fail";
+function setBrowserRules(blockStatus) {
+    console.log(blockStatus);
+    if (blockStatus == "block") {
+        let rule = {
+            id: 1,
+            priority: 1,
+            action: { type: "block" },
+            condition: {
+                urlFilter: "googlevideo",
+                domains: ["youtube.com"],
+            },
+        };
+
+        chrome.declarativeNetRequest.updateDynamicRules({
+            addRules: [rule],
+            removeRuleIds: [1],
+        });
+    } else if (blockStatus == "allow") {
+        chrome.declarativeNetRequest.updateDynamicRules({
+            removeRuleIds: [1],
+        });
     }
-    if (new RegExp(/[~`!#$%\^&*+=\[\]\\';,/{}|\\":<>\?]/g).test(trimUrl.slice(2))) {
-        return "Fail"
-    } else {
-        return trimUrl
+}
+
+function trimYoutubeUrl(url) {
+    if (youtubeVideoPattern.test(url)) {
+        return url.slice(url.search("v="))
+    } else if (url.length == 13 && url.includes("v=")) {
+        if (new RegExp(/[~`!#$%\^&*+=\[\]\\';,/{}|\\":<>\?]/g).test(url.slice(2))) { //prevent v=...&c=...
+            return "Fail"
+        } else {
+            return url
+        }
+    } else {// Not youtube
+        return "Fail";
     }
 }
 
@@ -160,55 +161,20 @@ function storagePut(url, block = true) {
 // Fetches radio button state from storage with default
 async function radioChangeListener() {
     return await readLocalStorage("radio").then(data => {
-        console.log(data);
         return data.toLowerCase();
     }).catch(() => {
         return "split";
     })
 }
 
-function setBrowserRules(blockStatus) {
-    console.log(blockStatus);
-    if (blockStatus == "block") {
-        let rule = {
-            id: 1,
-            priority: 1,
-            action: { type: "block" },
-            condition: {
-                urlFilter: "googlevideo",
-                domains: ["youtube.com"],
-            },
-        };
-
-        chrome.declarativeNetRequest.updateDynamicRules({
-            addRules: [rule],
-            removeRuleIds: [1],
-        });
-        console.log(rule);
-    } else if (blockStatus == "allow") {
-        chrome.declarativeNetRequest.updateDynamicRules({
-            removeRuleIds: [1],
-        });
-    }
-}
-
-async function getCurrentTab() {
-    let queryOptions = { active: true, currentWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
-}
-
-
 /**************LISTENERS************************/
-
-browser.tabs.onActivated.addListener(async () => {
-    console.log("onActivated");
+//because blockCheck and greenTab have self contained logic, they can run without await-ing
+browser.tabs.onActivated.addListener(() => {
     write2Browser();
     blockCheck();
     greenTab();
 });
 browser.tabs.onUpdated.addListener(() => {
-    console.log("onUpdated");
     write2Browser();
     blockCheck();
     greenTab();
@@ -217,80 +183,6 @@ browser.tabs.onUpdated.addListener(() => {
 if (navigator.userAgent.indexOf("Chrome") != -1) {
     console.log("Using Chrome");
 } else {
-    console.log("firefox detected, this branch compatible with chrome");
+    console.log("firefox detected, this should cause a manifest version error");
 
 }
-
-//****************Code That Doesn't Work******************************* */
-
-// async function onOptionsMessage(message, sender, sendResponse) {
-//     console.log(message);
-//     let url = message?.data;
-//     let videoTitle = await noAPITitleFromUrl(url);
-//     console.log(videoTitle);
-//     console.log(videoTitle + " is the title");
-//     sendResponse({ title: videoTitle })
-// }
-
-// async function noAPITitleFromUrl(url) {
-//     console.log("noAPITitleFromUrl : background.js : " + url);
-//     let title = "";
-//     var response = await fetch(url);
-//     switch (response.status) {
-//         // status "OK"
-//         case 200:
-//             let result = await response.blob()
-//             console.log(result);
-//             await result.text().then(text => {
-//                 title = text.slice(text.indexOf("<title>") + 7, text.indexOf("</title>"));
-//             });
-//             console.log(title);
-//             return title;
-//         // status "Not Found"
-//         case 404:
-//             console.log('Not Found');
-//             return `"${url}" is not a valid video`;
-//     }
-// }
-
-// async function titleFromCurrentPage() {
-//     return new Promise((resolve, reject) => {
-//         let title;
-//         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//             if (tabs[0] != null) {
-//                 chrome.tabs.sendMessage(tabs[0]?.id, { title: true }, function (response) {
-//                     console.log(response);
-//                     title = response?.title;
-//                     console.log(title);
-//                 });
-//             }
-//         });
-//         resolve(title);
-
-//     });
-// }
-
-
-
-
-
-
-//check url using tabs,
-//apply rule
-//detect changes in url
-//add or remove rule...
-//Lol he says working with an extension is painful
-//Allow you to work on it without the browser extension
-
-//Name
-//Folder based blocking
-
-
-
-
-// Enumerated changes from main : f36cbef Merge branch videoTitles
-// Get video titles from fetch request, not API
-// Change Proxy logic from every request to onTabChange
-// Change storage data structure
-// Element cover for blocked pages
-// Is the best option to merge?????? -> could be.....
